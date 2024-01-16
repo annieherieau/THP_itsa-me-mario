6603c11142cb68f788a25c506930d133c474a696
annieherieau <annie.herieau@gmail.com>
2024-01-16 13:40:31 +0100

    phase selon conditions

--- exo_12.rb
+++ exo_12.rb
@@ -1,5 +1,3 @@
-
-
 # 2.11. Virer les années
 
 actual_year = 2024
@@ -7,18 +5,29 @@ age = 0
 
 puts "Quel âge as-tu ?"
 print "> "
-user_age = Integer(gets.chomp)
+user_age = gets.chomp.to_i
 
 user_yearOfBirth = actual_year-user_age
 numberOfYears = user_age
 
+# créer la phase de façon conditionnelle
 for year in user_yearOfBirth..actual_year
-    if numberOfYears == age
-        puts "#{year} : Il y a #{numberOfYears} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
+    answer = "#{year} : "
+
+    # indique les années si != 0
+    numberOfYears != 0 ? answer += "Il y a #{numberOfYears} ans, " : answer += "Cette année, "
+    
+    # condition sur l'âge
+    case age
+    when 0
+        answer += "tu es né(e)"
+    when numberOfYears
+        answer += "tu avais la moitié de l'âge que tu as aujourd'hui"
     else
-        puts "#{year} : Il y a #{numberOfYears} ans, tu avais #{age} an(s)"
+        answer += "tu avais #{age} an(s)"
     end
-    year +=1
+
+    puts answer
     numberOfYears -=1
     age +=1
 end
